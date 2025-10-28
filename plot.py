7742cf812900c014fa5b95d474b1f2ea2e32c224
halchil <halz.create1215@gmail.com>
2025-10-28 14:48:30 +0900

    commit

--- plot.py
+++ plot.py
@@ -1,2 +1,36 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+a = np.array([1,2,3])
+word = "haru1"
+
 print("Hello Haru!!")
 
+print(a)
+
+if word == "haru":
+    print("word is haru")
+
+for i in range(1,10+1,1):
+    print("値は",i,"です。")
+
+# numpyで読み込み
+# data = np.loadtxt("Python-Plot/data/data.csv",delimiter=",",skiprows=1)
+#data = np.genfromtxt("Python-Plot/data/data.csv", delimiter=",", dtype=None, encoding="utf-8", names=True)
+# print(data)
+
+# Pandasで読み込み
+df = pd.read_csv("Python-Plot/data/data.csv")
+print(df.head)
+
+plt.plot(df['ID'],df['colum1'],marker='o')
+# plt.show()
+
+folder = "Python-Plot/graphs"
+
+# 保存
+filepath = os.path.join(folder, "id_vs_colum1.png")
+plt.savefig(filepath,dpi=300)
+plt.close() # 画面に表示せずに閉じる
